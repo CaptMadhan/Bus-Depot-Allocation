c22d33839604faf940626344f48a8fe76e67c517
Kiran jacky <58543460+Kiranjacky@users.noreply.github.com>
2022-01-12 17:53:49 +0530

    Demo11

--- root.py
+++ root.py
@@ -11,49 +11,54 @@ data_base = base.connect("demo1.db")
 cursor = data_base.cursor()
 
 # Define a function for opening the file
-def open_file():
-   filename = filedialog.askopenfilename(title="Open a File", filetype=(("xlxs files", ".*xlsx"),
-("All Files", "*.")))
 
-   if filename:
-      try:
-         filename = r"{}".format(filename)
-         df = pd.read_excel(filename,header=None)
-         df.to_sql('Data', data_base, if_exists='replace',index = False)
-         cursor.execute('''SELECT * FROM Data''')
-         for row in cursor.fetchall():
-            print (row)
-         #print(df)
-      except ValueError:
-         messagebox.showerror('Error', 'File could not be opened')
-      except FileNotFoundError:
-         messagebox.showerror('Error',"File Not Found")
+
+def open_file():
+    filename = filedialog.askopenfilename(title="Open a File", filetype=(("xlxs files", ".*xlsx"),
+                                                                         ("All Files", "*.")))
+
+    if filename:
+        try:
+            filename = r"{}".format(filename)
+            df = pd.read_excel(filename, header=None)
+            df.to_sql('Data', data_base, if_exists='replace', index=False)
+            cursor.execute('''SELECT * FROM Data''')
+            for row in cursor.fetchall():
+                print(row)
+            # print(df)
+        except ValueError:
+            messagebox.showerror('Error', 'File could not be opened')
+        except FileNotFoundError:
+            messagebox.showerror('Error', "File Not Found")
+
 
 def show_data():
-   cursor.execute('''SELECT * FROM Data''')
-   data = pd.DataFrame(cursor.fetchall())    
-   #Warehouse data only -->Change variables later
-   ware = list(data.iloc[0,:])
-   ware = ware[1:]
+    cursor.execute('''SELECT * FROM Data''')
+    data = pd.DataFrame(cursor.fetchall())
+    # Warehouse data only -->Change variables later
+    ware = list(data.iloc[0, :])
+    ware = ware[1:]
 
-   #Factory data only 
-   fact = list(data.iloc[:,0])
-   fact = fact[1:]
-   
-   #Weights
-   weights = np.array(data.iloc[1:,1:])
-   
-   print("warehouse: ",ware)
-   print("Factory: ",fact)
-   print("Weights:\n",weights)
+    # Factory data only
+    fact = list(data.iloc[:, 0])
+    fact = fact[1:]
+
+    # Weights
+    weights = np.array(data.iloc[1:, 1:])
+
+    print("warehouse: ", ware)
+    print("Factory: ", fact)
+    print("Weights:\n", weights)
+
 
 main_page = Tk()
 main_page.title('Bus Depot Allocation')
-main_page.configure(bg= "#e7f0fd")
+main_page.configure(bg="#e7f0fd")
 main_page.geometry("1200x800")
-upload=Button(main_page,text="Upload file",padx=20,pady=3,command=open_file)
-upload.grid(row=1,column=1,pady=8,padx=20)
-display_data=Button(main_page,text="Display data",padx=20,pady=3,command=show_data)
-display_data.grid(row=2,column=1,pady=8,padx=20)
+upload = Button(main_page, text="Upload file",
+                padx=20, pady=3, command=open_file)
+upload.grid(row=1, column=1, pady=8, padx=20)
+display_data = Button(main_page, text="Display data",
+                      padx=20, pady=3, command=show_data)
+display_data.grid(row=2, column=1, pady=8, padx=20)
 mainloop()
-# Hello World
