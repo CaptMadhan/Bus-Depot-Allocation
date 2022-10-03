ce208e2dedda4cb64c34577673eef32877fea85a
Madhan Murugan <91775905+Capt-Madhan@users.noreply.github.com>
2022-10-03 13:55:30 +0530

    Added file description for create_table.py (#7)

--- create_table.py
+++ create_table.py
@@ -1,3 +1,10 @@
+'''
+    create_table.py file is responsible to create table "Data" and "Backup_Data" if it did not exists.
+
+    This file is run only in the initial stage i.e. when the project is run for the first time.
+    "Data" table is used to store all the values needed for the project run, and is accessed when needed.
+    "Backup_Data" table is used as a backup wherein no read or write operation is done, to secure from processing error. 
+'''
 import sqlite3 as base
 
 # create or connect a data base
